9b3842236823da5b55cc7fd7e142302be1d8f0de
shreyas494 <shreyaskulkarni850@gmail.com>
2025-03-21 11:24:28 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let name="shreyas"
+let age=18
+let city=null;
+let division;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof division);
+
+//null value is displayed as object 
+
