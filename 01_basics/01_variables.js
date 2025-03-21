54617f3aad953303c6f760271f8e93cb60d72c88
shreyas494 <shreyaskulkarni850@gmail.com>
2025-03-21 08:35:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+let email="sk@gmail.com"
+const username="shreyas"
+var phno="1234567890" // use of var is avoided in new practices so avoid using it.
+console.log(email);
+console.log(username);
+console.table([email,username])
+
