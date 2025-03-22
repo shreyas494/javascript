72a56f954d7d75c97e4abde8bf5119057c278c84
shreyas494 <shreyaskulkarni850@gmail.com>
2025-03-22 14:01:37 +0000

    nums and maths

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log("2"==2);
+// console.log("2"===2);
+
+// const gamename=new String("shreyasinc")
+// console.log(gamename[0]);
+// console.log(gamename[1]);
+
+const url="https://www.google2024.com"
+let nurl=url.replace('2024','-')
+console.log(nurl)
+
