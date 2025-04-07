ee2e77d32de05f01809086c2f2582aa4320e5dee
shreyas494 <shreyaskulkarni850@gmail.com>
2025-04-07 13:35:26 +0000

    arrow functions

---
+++ 03_basics/arrow_functions.js
@@ -0,0 +1,30 @@
+// const user=
+// {
+//     username : 'hitesh',
+//     price : 999,
+//     welcomemessage:function()
+//     {
+//         console.log(`${this.username} welcome to wwebsite`);
+//         console.log(this);
+        
+        
+//     }
+// }
+
+// user.welcomemessage();
+// user.username='shreyas'
+// user.welcomemessage();
+
+// console.log(this);
+
+
+// const add = (num1,num2) => (num1+num2)
+// console.log(username(3,4));
+
+
+const username= (num1,num2) => ({
+    name:'hitesh'
+    
+})
+
+console.log(username(3,4));
